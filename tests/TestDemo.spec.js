4e25727796b43ebaf75cf8c4d67e9921c7852951
Nisha Singh <NishaS@teamblr.com>
2024-06-03 10:30:58 +0530

    asddf

--- tests/TestDemo.spec.js
+++ tests/TestDemo.spec.js
@@ -6,7 +6,7 @@ test('test', async ({ page }) => {slowmo : 800
   await page.getByPlaceholder('Password').fill('18@december');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.goto('https://alcsaws.teamlease.com/GUI/Login/Home.aspx');
-  await page.setTimeout(3000)
+  await page.waitForTimeout(10000);
   await page.click("//i[@class='material-icons powerOff']");
   await page.pause();
 
